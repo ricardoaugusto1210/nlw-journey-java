c9b3bcaf6f2ca36bfe7d94592676dde9f925df76
ricardoaugusto.eb@gmail.com <ricardoaugusto.eb@gmail.com>
2024-07-09 21:30:34 -0300

    feat: chore class Trip implements constructor

--- src/main/java/com/rocketseat/planner/trip/Trip.java
+++ src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -1,15 +1,29 @@
 package com.rocketseat.planner.trip;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Entity
+//Mapeia o nome da tabela caso não seja o mesmo da classe
+@Table(name = "trips")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trip {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
 
     @Column(name = "starts_at", nullable = false)
     private LocalDateTime startsAt;
@@ -21,10 +35,17 @@ public class Trip {
     private Boolean isConfirmed;
 
     @Column(name = "owner_name", nullable = false)
-    private Boolean ownerName;
+    private String ownerName;
 
     @Column(name = "owner_email", nullable = false)
-    private Boolean ownerEmail;
+    private String ownerEmail;
 
-
+    public Trip(TripRequestPayload data){
+        this.destination = data.destination();
+        this.isConfirmed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+    }
 }
