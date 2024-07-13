e6ea3dd4c59953558ee1b87777bb81aeba75cf7b
ricardoaugusto.eb@gmail.com <ricardoaugusto.eb@gmail.com>
2024-07-12 21:03:42 -0300

    feat: aula3

--- src/main/resources/db/migration/V2__create-table-participant.sql
+++ src/main/resources/db/migration/V2__create-table-participant.sql
@@ -5,5 +5,5 @@ CREATE TABLE participants (
     is_confirmed BOOLEAN NOT NULL,
 --    chave estrangeira
     trip_id UUID,
-    FOREIGN KEY (trip_id) REFERENCES trips(id)
+    FOREIGN KEY (trip_id) REFERENCES trips ON DELETE CASCADE
 );
